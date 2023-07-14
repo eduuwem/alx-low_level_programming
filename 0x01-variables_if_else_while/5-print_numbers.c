fd328ae378dc526e6889a579de0e33e768a74244
eduuwem <eduuwem.udoh@gmail.com>
2023-07-14 09:46:55 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - inputs
+ * Decription: printing base 10 numbers
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
