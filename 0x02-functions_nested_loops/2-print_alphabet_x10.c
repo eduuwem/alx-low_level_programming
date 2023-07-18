0b25462836fb768761d155f405a51bf4f1aa275a
eduuwem <eduuwem.udoh@gmail.com>
2023-07-18 08:26:05 -0700

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - alphabet 10 times in lowercase
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+		_putchar(ch);
+		}
+		{
+		_putchar('\n');
+		i++;
+		}
+	}
+}
