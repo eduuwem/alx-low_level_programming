76ca97bb991cb88af4449306e3d5e7d5ee995bf3
eduuwem <eduuwem.udoh@gmail.com>
2023-07-18 17:14:54 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,10 +10,10 @@ int main(void)
 	int sum = 0;
 	int i;
 
-	for (i = 0; i < 1024; i++)
+	for (i = 1; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
-			sum = sum + 1;
+			sum = sum + i;
 	}
 	printf("%d\n", sum);
 	return (0);
