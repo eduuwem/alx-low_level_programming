76f6a17cffbd56c32781e80ad57e22979b399e0f
eduuwem <eduuwem.udoh@gmail.com>
2023-07-24 12:12:11 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
+ * _atoi - convert a string to an integer
  * @s: string to be converted
  *
  * Return: the int converted from the string
