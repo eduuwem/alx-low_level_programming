72980402ea37ae627d69ed69082fcf42a0b2095e
eduuwem <eduuwem.udoh@gmail.com>
2023-08-14 06:25:50 -0700

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define _DOG_H_
 
 /**
- * struct dog - struct that create a puppy information
+ * struct dog - struct that creates a puppy information
  * @name: the name of the dog
  * @age: dog age
  * @owner: the owner of the dog
