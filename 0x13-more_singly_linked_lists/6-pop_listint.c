cdc1460677154a30d0375f01e42135ca061722d4
eduuwem <eduuwem.udoh@gmail.com>
2023-08-28 18:42:33 -0700

    update

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * pop_listint - function to deletes the head node of a
+ * linked list
+ * @head: pointer to the head of the nodes
+ * linked_t list
+ *
+ * Return: the head node’s data (n), 0 if the list is empty
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *count,*temp;
+	int x;
+
+	if (head == NULL)
+		return (0);
+	
+	temp = count = *head;
+	if (*head)
+	{
+		x = count->n;
+		*head = count->next;
+		free(temp);
+	}
+	else
+		x = 0;
+	return (x);
+}
+
